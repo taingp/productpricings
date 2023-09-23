7472c592baa3458989b6398656f60e9d94de9d65
APlatDev <taingpengsreng@atech-it.com>
2023-09-23 22:07:20 +0700

    Updating projects for ProductPricings-Solution1

---
+++ MenuLib/Menu.cs
@@ -0,0 +1,6 @@
+﻿namespace MenuLibrary;
+public class Menu
+{
+    public string Text { get; set; } = "";
+    public Action Action { get; set; } = default!;
+}
---
+++ MenuLib/MenuBank.cs
@@ -0,0 +1,50 @@
+﻿namespace MenuLibrary;
+public class MenuBank
+{
+    public string Title { get; set; } = "";
+    public string ChosenText { get; set; } = "Index";
+    public string LeadingText { get; set; } = "  ";
+    public List<Menu> Menues { get; set; } = new();
+
+    public void Show()
+    {
+        Console.WriteLine($"[{Title} menu]");
+        for(int k=0; k<Menues.Count; k++)
+        {
+            var menu = Menues[k];
+            Console.WriteLine($"{k + 1,3}-{menu.Text}");
+        }
+    }
+    public Menu GetMenu()
+    {
+        int input = Input(1, Menues.Count, ChosenText, LeadingText);
+        return Menues[input - 1];
+    }
+
+    public void MenuSimulate(Action? leadingAction=null)
+    {
+        while (true)
+        {
+            if (leadingAction != null) leadingAction();
+            Show();
+            var actingMenu = GetMenu();
+            actingMenu.Action();
+        }
+    }
+    public static int Input(int lower, int upper, string text, string leading="")
+    {
+        while (true)
+        {
+            Console.Write($"{leading}{text}({lower}-{upper}): ");
+            if (int.TryParse(Console.ReadLine(), out var input) == false)
+            {
+                Console.WriteLine($"{leading}>Invalid input format");
+                continue;
+            }
+            if (lower <= input && input <= upper) return input;
+            Console.WriteLine($"{leading}>The input is {input}, the input must be in [{lower}, {upper}]");
+        }
+    }
+
+    
+}
